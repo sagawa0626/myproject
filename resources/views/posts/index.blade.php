6a875a7f99013bd49724cfabe171bff0c1dc3401
佐川嵩 <sagataka@sagawakasanoMacBook-Air.local>
2020-08-27 19:23:08 +0900

    [update] インデント修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,51 +27,51 @@
                                     </div>
                                 </div>
                                 @if( Auth::id() === $post->user_id )
-                                <!-- dropdown -->
-                                <div class="ml-auto card-text">
-                                    <div class="dropdown">
-                                        <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <button type="button" class="btn btn-link text-muted m-0 p-2">
-                                                <i class="fas fa-ellipsis-v"></i>
-                                            </button>
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right">
-                                            <a class="dropdown-item" href="{{ route('posts.edit', ['post' => $post]) }}">
-                                                <i class="fas fa-pen mr-1"></i>投稿を更新する
-                                            </a>
-                                            <div class="dropdown-divider"></div>
-                                                <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $post->id }}">
-                                                    <i class="fas fa-trash-alt mr-1"></i>投稿を削除する
-                                                </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- dropdown -->
-
-                                <!-- modal -->
-                                <div id="modal-delete-{{ $post->id }}" class="modal fade" tabindex="-1" role="dialog">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                                                    <span aria-hidden="true">×</span>
+                                    <!-- dropdown -->
+                                    <div class="ml-auto card-text">
+                                        <div class="dropdown">
+                                            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                <button type="button" class="btn btn-link text-muted m-0 p-2">
+                                                    <i class="fas fa-ellipsis-v"></i>
                                                 </button>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item" href="{{ route('posts.edit', ['post' => $post]) }}">
+                                                    <i class="fas fa-pen mr-1"></i>投稿を更新する
+                                                </a>
+                                                <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $post->id }}">
+                                                        <i class="fas fa-trash-alt mr-1"></i>投稿を削除する
+                                                    </a>
                                             </div>
-                                            <form method="POST" action="{{ route('posts.destroy', ['post' => $post]) }}">
-                                                @csrf
-                                                @method('DELETE')
-                                                <div class="modal-body">
-                                                    {{ $post->title }}を削除します。よろしいですか？
-                                                </div>
-                                                <div class="modal-footer justify-content-between">
-                                                    <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                                                    <button type="submit" class="btn btn-danger">削除する</button>
-                                                </div>
-                                            </form>
                                         </div>
                                     </div>
-                                </div>
-                                <!-- modal -->
+                                    <!-- dropdown -->
+
+                                    <!-- modal -->
+                                    <div id="modal-delete-{{ $post->id }}" class="modal fade" tabindex="-1" role="dialog">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                                                        <span aria-hidden="true">×</span>
+                                                    </button>
+                                                </div>
+                                                <form method="POST" action="{{ route('posts.destroy', ['post' => $post]) }}">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <div class="modal-body">
+                                                        {{ $post->title }}を削除します。よろしいですか？
+                                                    </div>
+                                                    <div class="modal-footer justify-content-between">
+                                                        <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                                                        <button type="submit" class="btn btn-danger">削除する</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- modal -->
                                 @endif
                             </div>
                             <div class="card" style="width: 100%;">
